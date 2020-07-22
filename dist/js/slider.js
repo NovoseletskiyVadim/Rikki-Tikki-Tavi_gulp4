d45962819d1e79e22f31bbdc53e0fb84e373d279
Vadim <novoseletskiy.v@gmail.com>
2020-07-22 14:46:28 +0300

    Building...

--- dist/js/slider.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function(){
-    $('.slider').glide({
-      autoplay: 3500,
-      hoverpause: true, // set to false for nonstop rotate
-      arrowRightText: '&rarr;',
-      arrowLeftText: '&larr;'
-    });
-});
